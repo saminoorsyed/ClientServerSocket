a79b480c8261a95797f6650c4313c07c46b8c875
saminoorsyed <saminoorsyed@gmail.com>
2023-06-07 22:26:43 -0400

    finish debugging double recv issue, need to add nicities to game

--- client.py
+++ client.py
@@ -1,88 +1,97 @@
 import socket
 
-def start_client():
+def client():
     # Create a socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Connect to the server at localhost and port xxxx
+    # Connect to the server
     server_address = ('localhost', 1234)
     client_socket.connect(server_address)
-    print("Connected to:", server_address)
+    # use try in case there are any errors
+    try:
+        while True:
+            # Prompt for message
+            message = input("Enter Input > ")
 
-    while True:
-        # Prompt for a message
-        message = input("Enter Input > ")
+            # Check if the message is '/q' to quit
+            if message.strip() == "/q":
+                break
 
-        # Send the message to the server
-        client_socket.send(message.encode())
+            # Check if the message is 'play tic-tac-toe' to initiate the game
+            if message.strip() == "play tic-tac-toe":
+                # Send the message to the server
+                client_socket.send(message.encode())
+                # Start the Tic-Tac-Toe game
+                play_tic_tac_toe(client_socket)
+                continue
 
-        # Check if the message is '/q' to quit
-        if message.strip() == "/q":
-            break
-        # check if message initiates tic-tac-toe
-        if message.strip().lower()=="play tic-tac-toe":
-            play_tic_tac_toe(client_socket)
-            break
-        # Receive data from the server
-        data = client_socket.recv(4096).decode()
+            # Send the message to the server
+            client_socket.send(message.encode())
 
-        # Print the received data
-        print("Received:", data)
+            # Receive the reply from the server
+            reply = client_socket.recv(4096).decode()
+            print("Server:", reply)
 
-        # Check if the server wants to play tic-tac-toe
-        if data.strip().lower() == "play tic-tac-toe":
-            play_tic_tac_toe(client_socket)
-            break
+            if reply.strip() == "play tic-tac-toe":
+                # Start the Tic-Tac-Toe game
+                play_tic_tac_toe(client_socket)
+                continue
 
-    client_socket.close()
-    print("Connection closed.")
-
+    finally:
+        # Close the socket
+        client_socket.close()
 
 def play_tic_tac_toe(client_socket):
     # Initialize the tic-tac-toe board
     board = [' '] * 9
-
+    possible_moves = [ "0", "1", "2", "3", "4", "5", "6", "7", "8","/q"]
     while True:
         # Print the current board
         print_board(board)
 
         # Prompt for a move
         reply = input("Enter Input (0-8) > ")
-
+        # assure that the reply is an integer between 0 and 8
+        while reply not in possible_moves:
+            print("invalid response")
+            reply = input("Enter Input (0-8) > ")
+        
+        possible_moves.remove(reply)
         # Send the move to the server
         client_socket.send(reply.encode())
-
+        if reply == "/q":
+            break
         # Update the board with the player's move
         index = int(reply)
+
         board[index] = 'X'
 
         # Check if the player won or the board is full
         if check_winner(board, 'X'):
-            reply = "You win!"
-            client_socket.send(reply.encode())
+            print_board(board)
+            print("you won!")
             break
         elif is_board_full(board):
-            reply = "It's a draw!"
-            client_socket.send(reply.encode())
+            print("draw game")
             break
 
         # Receive the opponent's move
         opponent_move = client_socket.recv(4096).decode()
-        if not opponent_move:
+        if not opponent_move or opponent_move == "/q":
             break
-
+        # remove opponent move from possible moves
+        if opponent_move in possible_moves:
+            possible_moves.remove(opponent_move)
         # Update the board with the opponent's move
         index = int(opponent_move)
         board[index] = 'O'
-
+        print_board(board)
         # Check if the opponent won or the board is full
         if check_winner(board, 'O'):
-            reply = "You win!"
-            client_socket.send(reply.encode())
+            print("the server won!")
             break
         elif is_board_full(board):
-            reply = "It's a draw!"
-            client_socket.send(reply.encode())
+            print("draw game")
             break
 
 
@@ -110,4 +119,4 @@ def is_board_full(board):
 
 
 if __name__ == '__main__':
-    start_client()
+    client()
--- server.py
+++ server.py
@@ -1,101 +1,120 @@
 import socket
 
-def start_server():
+def server():
     # Create a socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Set socket option to reuse the address
+    # Set socket option to reuse address
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # Bind the socket to localhost and port xxxx
+    # Bind to localhost and port
     server_address = ('localhost', 1234)
     server_socket.bind(server_address)
 
     # Listen for connections
     server_socket.listen(1)
-    print("Server is listening on {}:{}".format(*server_address))
 
-    while True:
-        # Wait for a connection
-        print("Waiting for a connection...")
-        client_socket, client_address = server_socket.accept()
-        print("Connected to:", client_address)
+    print("Waiting for a connection...")
 
-        # Handle the chat until the game is initiated
+    # Accept a connection
+    client_socket, client_address = server_socket.accept()
+    print("Connected to:", client_address)
+
+    try:
         while True:
             # Receive data from the client
             data = client_socket.recv(4096).decode()
+
             if not data:
                 break
 
-            print("Received:", data)
+            print("Client:", data)
 
-            # Check if the client wants to play tic-tac-toe
-            if data.strip().lower() == "play tic-tac-toe":
-                play_tic_tac_toe(client_socket)
+            # Check if the data is '/q' to quit
+            if data.strip() == "/q":
                 break
 
-            # Prompt for a reply
+            # Check if the data is 'play tic-tac-toe' to initiate the game
+            if data.strip() == "play tic-tac-toe":
+                # Start the Tic-Tac-Toe game
+                play_tic_tac_toe(client_socket)
+                continue
+
+            # Prompt for reply
             reply = input("Enter Input > ")
 
             # Send the reply to the client
             client_socket.send(reply.encode())
-            if reply.strip().lower()=="play tic-tac-toe":
+
+            if reply.strip() == "play tic-tac-toe":
+                # Start the Tic-Tac-Toe game
                 play_tic_tac_toe(client_socket)
-                break
+                continue
+
             # Check if the reply is '/q' to quit
             if reply.strip() == "/q":
                 break
 
+    finally:
+        # Close the sockets
         client_socket.close()
-        print("Connection closed.")
-
+        server_socket.close()
 
 def play_tic_tac_toe(client_socket):
     # Initialize the tic-tac-toe board
     board = [' '] * 9
+    possible_moves = [ "0", "1", "2", "3", "4", "5", "6", "7", "8","/q"]
 
     while True:
         # Receive the opponent's move
         opponent_move = client_socket.recv(4096).decode()
-        if not opponent_move:
+        if not opponent_move or opponent_move == "/q":
             break
-
+        if opponent_move in possible_moves:
+            possible_moves.remove(opponent_move)
         # Update the board with the opponent's move
         index = int(opponent_move)
         board[index] = 'O'
 
-        # Check if the opponent won or the board is full
-        if check_winner(board, 'O'):
-            reply = "You win!"
-            client_socket.send(reply.encode())
-            break
-        elif is_board_full(board):
-            reply = "It's a draw!"
-            client_socket.send(reply.encode())
-            break
-
         # Print the updated board
         print_board(board)
 
+        # Check if the opponent won or the board is full
+        if check_winner(board, 'O'):
+            print("the client won")
+            break
+        elif is_board_full(board):
+            print("draw game")
+            break
+
+
         # Prompt for a move
         reply = input("Enter Input (0-8) > ")
-
+        # assure that the reply is an integer between 0 and 8
+        while reply not in possible_moves:
+            print("invalid response")
+            reply = input("Enter Input (0-8) > ")
+        possible_moves.remove(reply)
         # Send the move to the client
         client_socket.send(reply.encode())
-
+        
+        if reply == "/q":
+            break
+        
         # Update the board with the player's move
         index = int(reply)
         board[index] = 'X'
 
         # Check if the player won or the board is full
         if check_winner(board, 'X'):
-            reply = "You win!"
-            client_socket.send(reply.encode())
+            # Print the updated board
+            print_board(board)
+            print("You won!")
             break
         elif is_board_full(board):
-            reply = "It's a draw!"
-            client_socket.send(reply.encode())
+            # Print the updated board
+            print_board(board)
+            print("draw game")
             break
 
 
@@ -123,4 +142,4 @@ def is_board_full(board):
 
 
 if __name__ == '__main__':
-    start_server()
+    server()
